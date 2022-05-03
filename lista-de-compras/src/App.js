572c0cb37c9f64194e6d538c8d110efba3211bf5
sarahrubia <sarahrubia22@gmail.com>
2022-05-03 05:55:39 -0300

    lista de compras primeira versao

--- lista-de-compras/src/App.js
+++ lista-de-compras/src/App.js
@@ -1,21 +1,13 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Lista from './components/Lista'
 
-const elemento = document.getElementById("root")
-
-class App extends Component {
-  ReactDOM.render(
-  {
-    var listaCompras = ['Alface', 'Tomate', 'Queijo', 'Macarrão', 'Arroz'];
-    var listaTarefas = ['Lavar louça', 'Varrer sala', 'Levar o lixo para fora', 'Estudar React'];
-
-    return (
+function App() {
+  return (
       <div className='App'>
-        <header>
-        <img src={logo} className='App-logo' alt='logo' />
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -28,51 +20,13 @@ class App extends Component {
           >
             Learn React
           </a>
-          
-          <Lista />
-          <h3>Lista de Compras</h3>
-          <Lista elementos={listaCompras} />
 
-          <h3>Lista de Tarefas</h3>
-          <Lista elementos={listaTarefas} />
+          <Lista />
+  
         </header>
-      </div>
-      , elemento
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
-
-// function App() {
-//   return (
-//       <div className='App'>
-//         <header className='App-header'>
-          // <img src={logo} className='App-logo' alt='logo' />
-          // <p>
-          //   Edit <code>src/App.js</code> and save to reload.
-          // </p>
-        
-          // <a
-          //   className='App-link'
-          //   href='https://reactjs.org'
-          //   target='_blank'
-          //   rel='noopener noreferrer'
-          // >
-          //   Learn React
-          // </a>
-    
-//           <Lista />
-//           <h3>Lista de Compras</h3>
-//           <Lista elementos={listaCompras} />
-
-//           <h3>Lista de Tarefas</h3>
-//           <Lista elementos={listaTarefas} />
-  
-//         </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 
--- lista-de-compras/src/components/Lista.js
+++ lista-de-compras/src/components/Lista.js
@@ -1,18 +1,33 @@
 import React from 'react';
 
- 
 const lista = (props) => {
-    console.log("[Lista.js] elementos: ", props.elementos);
     return (
         <div>
             <ul>
-                {props.elementos.map((item, index)=>{
-                    return <li key={'lista-item-'+item+'-'+index}>{item}</li>
-                })}
+                <li>Alface</li>
+                <li>Tomate</li>
+                <li>Queijo</li>
+                <li>Macarrão</li>
+                <li>Arroz</li>
             </ul>
         </div>
     )
+    };
+     
+    export default lista;
 
-};
+// const lista = (props) => {
+//     console.log("[Lista.js] elementos: ", props.elementos);
+//     return (
+//         <div>
+//             <ul>
+//                 {props.elementos.map((item, index)=>{
+//                     return <li key={'lista-item-'+item+'-'+index}>{item}</li>
+//                 })}
+//             </ul>
+//         </div>
+//     )
+
+// };
  
-export default lista;
+// export default lista;
